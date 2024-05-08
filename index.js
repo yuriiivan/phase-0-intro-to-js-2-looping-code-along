4d149dff4a493267a9da25a554cb408d4289fa64
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-05-08 10:22:36 -0400

    Deleted comments

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const writeCards = (array,event) => {
   const birthdayMessages = [];
-  //for(let i = 0; i < array.length; i++){
     for(let name of array){
     birthdayMessages.push(`Thank you, ${name}, for the wonderful ${event} gift!`);
   }
