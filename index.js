c8fb760dd32e67973f4b9363e0e458d2b67631b0
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-05-08 10:07:02 -0400

    Create birthday messages

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solutions in this file
+const writeCards = (array,event) => {
+  const birthdayMessages = [];
+  for(let i = 0; i < array.length; i++){
+    birthdayMessages.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+  }
+  return birthdayMessages;
+}
